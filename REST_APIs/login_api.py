56208473689180dfb5cf5e0fe512c7b7dfdfffd7
DepGkatz <despoina.gkatzioura@8bellsresearch.com>
2022-05-23 11:16:21 +0300

    Update sqlite_db's path in login_api.py

--- REST_APIs/login_api.py
+++ REST_APIs/login_api.py
@@ -42,7 +42,7 @@ class Login(Resource):
         r = requests.post(url=KEYROCK_DISCOVERY_URL+"/oauth2/token", headers=header, data=body, verify=False)
         json = r.json()
         token = json["access_token"]
-        user.User.update_field("email", email, "user", "token", token, path = "../")
+        user.User.update_field("email", email, "user", "token", token, path = "../../DFF_Web_App/")
         return token
 
 api.add_resource(Login, '/login')
