8db8df3658c344f6672ee25e43fb812531fb49ba
DepGkatz <despoina.gkatzioura@8bellsresearch.com>
2022-06-10 11:29:11 +0300

    add if not exists in sql schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE user IF NOT EXISTS(
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
@@ -8,13 +8,11 @@ CREATE TABLE user (
   domain_name TEXT
 );
 
-CREATE TABLE history (
+CREATE TABLE history IF NOT EXISTS(
 	history_id TEXT PRIMARY KEY,
   usr_id TEXT NOT NULL,
   timestamp TIMESTAMP NOT NULL,
   filename TEXT NOT NULL,
   description TEXT NOT NULL,
   foreign KEY (usr_id) references user (id)
-  
-	
 );
