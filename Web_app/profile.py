9f78766628072448fe3d2b24c638f3cfe0a0aaae
DepGkatz <despoina.gkatzioura@8bellsresearch.com>
2022-06-22 15:28:14 +0300

    bug corrected in profile.py

--- Web_app/profile.py
+++ Web_app/profile.py
@@ -47,7 +47,7 @@ def edit_profile():
                 for app in new_app_list:
                     if app not in temp_list:
                         if app not in all_apps:
-                            createRequest(appl, "http://quantumleap:8668/v2/notify")
+                            createRequest(app, "http://quantumleap:8668/v2/notify")
                         User.create_user_app(app, current_user.id)
 
                 User.update_field("id", current_user.id, "user", "domain_name", new_url)
