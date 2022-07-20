0f4443d393d0e38c06b97c055fb0275b9633ce17
DepGkatz <despoina.gkatzioura@8bellsresearch.com>
2022-07-20 15:35:59 +0300

    add mongo user

--- REST_APIs/oriondb.py
+++ REST_APIs/oriondb.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
+import os 
 
-# USER = os.getenv("USER")
-# PASS = os.getenv("PASS")
 DB_URL = "mongodb://cloud-20-nic.8bellsresearch.com:27017/"
+MONGO_DB_PASS=os.getenv("MONGO_DB_PASS")
 
 def mongoConnect(database):
-    client = MongoClient(DB_URL+database, directConnection=True)
+    client = MongoClient(DB_URL, username='user', password=MONGO_DB_PASS, authSource=database, directConnection=True)
     return client
   
 def mongoCloseConnection(client):
