17cdcb4d5ecaaccf7dd231078c28d51fd310d291
nsideras8 <nikos.sideras@8bellsresearch.com>
2022-09-23 12:03:21 +0300

    changed port 5007 to 8090

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def push_app_org():
 # EDW TI FASH?????
 if __name__ == "__main__":
     ipV4IP = socket.gethostbyname(socket.gethostname())
-    app.run(ssl_context="adhoc", host=ipV4IP, port=5007)
+    app.run(ssl_context="adhoc", host=ipV4IP, port=8090)
