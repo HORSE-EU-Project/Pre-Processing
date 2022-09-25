7f0f8ac81270daf29c4992f7954cd8e8a4dc7a55
nsideras8 <nikos.sideras@8bellsresearch.com>
2022-09-25 04:27:14 +0300

    one webserver 2sslnone

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ app.config.update({
     'TESTING': True,
     'DEBUG': True,
     'OIDC_CLIENT_SECRETS': 'client_secrets.json',
-    #'OIDC_CALLBACK_ROUTE': '/',
-    'OVERWRITE_REDIRECT_URI':'https://dff.8bellsresearch.com/*',
+    'OIDC_CALLBACK_ROUTE': '/*',
+    #'OVERWRITE_REDIRECT_URI':'https://dff.8bellsresearch.com/*',
     'OIDC_ID_TOKEN_COOKIE_SECURE': False,
     'OIDC_REQUIRE_VERIFIED_EMAIL': False,
     'OIDC_USER_INFO_ENABLED': True,
@@ -157,4 +157,5 @@ def push_app_org():
 # EDW TI FASH?????
 if __name__ == "__main__":
     ipV4IP = socket.gethostbyname(socket.gethostname())
-    app.run(ssl_context="adhoc", host=ipV4IP, port=8090)
+    #app.run(ssl_context="adhoc", host=ipV4IP, port=8090)
+    app.run(ssl_context=None, host=ipV4IP, port=8090)
