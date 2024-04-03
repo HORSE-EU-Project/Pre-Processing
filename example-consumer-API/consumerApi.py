c30297717c166b2bf8e1046bf64e27cc305909dd
midan171 <mikedan171@gmail.com>
2024-04-03 15:51:57 +0300

    DFF consumer app debugging

--- example-consumer-API/consumerApi.py
+++ example-consumer-API/consumerApi.py
@@ -9,13 +9,16 @@ class ReceiveDFF(Resource):
     def post(self):
         dff_data = request.get_json()
         if isinstance(dff_data, dict):
+            print("I received you data!")
             return {"message": "I received you data!"}, 200
-        else: 
+        else:
+            print("Data not in json format.") 
             return {"message": "Data not in json format."}, 400
 
 api.add_resource(ReceiveDFF, '/dff-data')
 
 
 if __name__ == '__main__':
-    ipV4IP = socket.gethostbyname(socket.gethostname())
+    ipV4IP = socket.gethost
+    byname(socket.gethostname())
     app.run(host="0.0.0.0")
