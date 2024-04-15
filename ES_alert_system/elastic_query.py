e5d19559446d11285dc917d3bbb1d313dac767ca
midan171 <mikedan171@gmail.com>
2024-04-15 12:39:43 +0300

    DFF Elastic custom approach

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime, timedelta
 
 class ElasticQuery:
-    def __init__(self,entity_type, es_url, query, endpoint, interval_minutes=1):
+    def __init__(self, entity_type, es_url, query, endpoint, interval_minutes=1, *args, **kwargs):
         self.entity_type = entity_type
         self.es_url = es_url
         self.query = query
