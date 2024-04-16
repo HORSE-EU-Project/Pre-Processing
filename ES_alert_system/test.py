0978f49ad59715e8ede7366c2ec99632667ed003
midan171 <mikedan171@gmail.com>
2024-04-16 15:19:59 +0300

    DFF Elastic custom approach Dockerized

--- ES_alert_system/test.py
+++ ES_alert_system/test.py
@@ -79,78 +79,78 @@ def main():
     queries = ConfigReader.read_config('./ES_alert_system/config.json')
     
     #Loop through the queries and run them
-    for query in queries:
-        print("Query ES URL: ", query.es_url)
-        print("Query Index: ", query.index)
-        print("Query Query: ", query.query)
-        print("Query Headers: ", query.headers)
-        print("Query Endpoint: ", query.endpoint)
-        print("Query Interval: ", query.interval)
-        print("Query Last Run: ", query.last_run)
-        print("====================================================")
+    # for query in queries:
+    #     print("Query ES URL: ", query.es_url)
+    #     print("Query Index: ", query.index)
+    #     print("Query Query: ", query.query)
+    #     print("Query Headers: ", query.headers)
+    #     print("Query Endpoint: ", query.endpoint)
+    #     print("Query Interval: ", query.interval)
+    #     print("Query Last Run: ", query.last_run)
+    #     print("====================================================")
         
-        results = query.run_query()
-        status_code = query.post_results(results)
-        query.print_results(results)
+    #     results = query.run_query()
+    #     status_code = query.post_results(results)
+    #     query.print_results(results)
     
     
     
     
     
-    # #every 10 seconds run the queries
-    # while True:
-    #     now = datetime.now()
-    #     for query in queries:
-    #         if not query.last_run or now >= query.last_run + query.interval:
-    #             results = query.run_query()
-    #             status_code = query.post_results(results)
-    #             if status_code == 200:
-    #                 logging.info("Query results successfully posted.")
-    #             else:
-    #                 logging.warning(f"Failed to post results: HTTP {status_code}")
-    #             query.last_run = now
+    #every 10 seconds run the queries
+    while True:
+        now = datetime.now()
+        for query in queries:
+            if not query.last_run or now >= query.last_run + query.interval:
+                results = query.run_query()
+                status_code = query.post_results(results)
+                if status_code == 200:
+                    logging.info("Query results successfully posted.")
+                else:
+                    logging.warning(f"Failed to post results: HTTP {status_code}")
+                query.last_run = now
         
         
     
-    # Define the Elasticsearch URL
-    es_url = "http://localhost:9200"
+    # # Define the Elasticsearch URL
+    # es_url = "http://localhost:9200"
     
-    # Define the index to query
-    index = "test_index"
+    # # Define the index to query
+    # index = "test_index"
     
-    # Define the query to execute
-    query = {
-        "query": {
-            "match_all": {}
-        }
-    }
+    # # Define the query to execute
+    # query = {
+    #     "query": {
+    #         "match_all": {}
+    #     }
+    # }
     
-    # Define the headers for the HTTP requests
-    headers = {"Content-Type": "application/json"}
+    # # Define the headers for the HTTP requests
+    # headers = {"Content-Type": "application/json"}
     
-    # Define the endpoint to post the results
-    endpoint = "http://localhost:5000/dff-data"
+    # # Define the endpoint to post the results
+    # endpoint = "http://localhost:5000/dff-data"
     
-    # Create an instance of the ElasticQuery class
-    query_instance = ElasticQuery(es_url, index, query, headers, endpoint)
+    # # Create an instance of the ElasticQuery class
+    # query_instance = ElasticQuery(es_url, index, query, headers, endpoint)
     
-    print("Query 2 ES URL: ", query_instance.es_url)
-    print("Query 2 Index: ", query_instance.index)
-    print("Query 2 Query: ", query_instance.query)
-    print("Query 2 Headers: ", query_instance.headers)
-    print("Query 2 Endpoint: ", query_instance.endpoint)
-    print("Query 2 Interval: ", query_instance.interval)
-    print("Query 2 Last Run: ", query_instance.last_run)
-    print("====================================================")
+    # print("Query 2 ES URL: ", query_instance.es_url)
+    # print("Query 2 Index: ", query_instance.index)
+    # print("Query 2 Query: ", query_instance.query)
+    # print("Query 2 Headers: ", query_instance.headers)
+    # print("Query 2 Endpoint: ", query_instance.endpoint)
+    # print("Query 2 Interval: ", query_instance.interval)
+    # print("Query 2 Last Run: ", query_instance.last_run)
+    # print("====================================================")
     
-    # Run the query
-    results = query_instance.run_query()
+    # # Run the query
+    # results = query_instance.run_query()
     
-    # Post the results
-    status_code = query_instance.post_results(results)
+    # # Post the results
+    # status_code = query_instance.post_results(results)
     
-    # Print the results
-    #query_instance.print_results(results)
+    # # Print the results
+    # #query_instance.print_results(results)
 
 if __name__ == "__main__":
     main()    
