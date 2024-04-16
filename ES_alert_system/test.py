b8210b180995f2b8fcfc30f42fe4296851423880
midan171 <mikedan171@gmail.com>
2024-04-16 15:02:52 +0300

    DFF Elastic custom approach Dockerized

--- ES_alert_system/test.py
+++ ES_alert_system/test.py
@@ -65,38 +65,69 @@ class ElasticQuery:
         else:
             logging.info("No results available to print.")
 
-    
+class ConfigReader:
+    @staticmethod
+    def read_config(config_file):
+        with open(config_file, 'r') as file:
+            data = json.load(file)
+            return [ElasticQuery(**item) for item in data["rules"]]
+
+  
 def main():
-    # Define the Elasticsearch URL
-    es_url = "http://localhost:9200"
+    #Read the configuration file
+    queries = ConfigReader.read_config('./ES_alert_system/config.json')
     
-    # Define the index to query
-    index = "test_index"
+    #Loop through the queries and run them
+    for query in queries:
+        results = query.run_query()
+        status_code = query.post_results(results)
+        query.print_results(results)
     
-    # Define the query to execute
-    query = {
-        "query": {
-            "match_all": {}
-        }
-    }
+    #every 10 seconds run the queries
+    while True:
+        now = datetime.now()
+        for query in queries:
+            if not query.last_run or now >= query.last_run + query.interval:
+                results = query.run_query()
+                status_code = query.post_results(results)
+                if status_code == 200:
+                    logging.info("Query results successfully posted.")
+                else:
+                    logging.warning(f"Failed to post results: HTTP {status_code}")
+                query.last_run = now
+        
+        
     
-    # Define the headers for the HTTP requests
-    headers = {"Content-Type": "application/json"}
+    # # Define the Elasticsearch URL
+    # es_url = "http://localhost:9200"
     
-    # Define the endpoint to post the results
-    endpoint = "http://localhost:5000/dff-data"
+    # # Define the index to query
+    # index = "test_index"
     
-    # Create an instance of the ElasticQuery class
-    query_instance = ElasticQuery(es_url, index, query, headers, endpoint)
+    # # Define the query to execute
+    # query = {
+    #     "query": {
+    #         "match_all": {}
+    #     }
+    # }
     
-    # Run the query
-    results = query_instance.run_query()
+    # # Define the headers for the HTTP requests
+    # headers = {"Content-Type": "application/json"}
     
-    # Post the results
-    status_code = query_instance.post_results(results)
+    # # Define the endpoint to post the results
+    # endpoint = "http://localhost:5000/dff-data"
     
-    # Print the results
-    query_instance.print_results(results)
+    # # Create an instance of the ElasticQuery class
+    # query_instance = ElasticQuery(es_url, index, query, headers, endpoint)
+    
+    # # Run the query
+    # results = query_instance.run_query()
+    
+    # # Post the results
+    # status_code = query_instance.post_results(results)
+    
+    # # Print the results
+    # query_instance.print_results(results)
 
 if __name__ == "__main__":
     main()    
