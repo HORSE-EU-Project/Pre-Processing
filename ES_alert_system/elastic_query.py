9f32ed1217d233626b82dea6fc2e6d4e8ad1ee88
midan171 <mikedan171@gmail.com>
2024-04-16 10:31:03 +0300

    DFF Elastic custom approach Dockerized

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -2,12 +2,12 @@ import requests
 from datetime import datetime, timedelta
 
 class ElasticQuery:
-    def __init__(self, entity_type, es_url, query, endpoint, interval_seconds=10, *args, **kwargs):
+    def __init__(self, entity_type, es_url, query, endpoint, interval=10, *args, **kwargs):
         self.entity_type = entity_type
         self.es_url = es_url
         self.query = query
         self.endpoint = endpoint
-        self.interval = timedelta(seconds=interval_seconds)
+        self.interval = timedelta(seconds=interval)
         self.last_run = None
 
     def run_query(self):
@@ -18,5 +18,9 @@ class ElasticQuery:
 
     def post_results(self, results):
         headers = {'Content-Type': 'application/json'}
+        print(results)
         response = requests.post(self.endpoint, headers=headers, json=results)
         return response.status_code
+
+    def print_results(self, results):
+        print(results)
