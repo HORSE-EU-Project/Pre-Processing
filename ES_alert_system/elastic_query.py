b2075d9fce6a3e7d7231f13eeb18875ca1421f0a
midan171 <mikedan171@gmail.com>
2024-04-16 13:01:09 +0300

    DFF Elastic custom approach Dockerized

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -12,7 +12,7 @@ class ElasticQuery:
             raise ValueError("All parameters must be provided and non-empty.")
 
         # Initialize Elasticsearch client with headers specific for Elasticsearch if needed
-        self.es = Elasticsearch([es_url])
+        self.es = Elasticsearch(es_url)
         self.index = index
         self.query = query
         self.endpoint = endpoint
