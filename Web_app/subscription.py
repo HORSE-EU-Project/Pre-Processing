da1cb263a7a4341c8ed7d409bf472fb1c2029156
midan171 <mikedan171@gmail.com>
2024-04-19 11:45:34 +0300

    Subscription Management sqlite changes

--- Web_app/subscription.py
+++ Web_app/subscription.py
@@ -53,13 +53,13 @@ def subscriptionSubmission():
         return redirect("/")
 
 
-@subscription.route('/subscribe/new_subscription', methods=['GET'])
+@subscription.route('/subscribe/new_subscription', methods=['GET'], endpoint='form_subscriptions')
 @decoratorCheckAppOrg
 def subscription_form():
     # Display the subscription form
     return render_template('create_subscription.html', name=current_user.name, email=current_user.email)
 
-@subscription.route('/subscribe/new', methods=['GET', 'POST'])
+@subscription.route('/subscribe/new', methods=['GET', 'POST'], endpoint='view_subscriptions')
 @decoratorCheckAppOrg
 def create_subscription():
     token = User.get_field("id", current_user.id, "user", "token")
