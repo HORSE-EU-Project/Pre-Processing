33143111b6d03df6804e2c6f010fc66f4eaa89fe
midan171 <mikedan171@gmail.com>
2024-04-22 10:15:03 +0300

    Subscription Management sqlite updates

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 -- Create the user table
 CREATE TABLE IF NOT EXISTS user(
   id TEXT PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  token TEXT UNIQUE NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  token TEXT NOT NULL,
   organization TEXT,
   domain_name TEXT
 );
@@ -12,8 +12,7 @@ CREATE TABLE IF NOT EXISTS user(
 CREATE TABLE IF NOT EXISTS apps(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
-  user_id TEXT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  user_id TEXT NOT NULL
 );
 
 -- Create the history table
@@ -22,36 +21,28 @@ CREATE TABLE IF NOT EXISTS history(
   usr_id TEXT NOT NULL,
   timestamp TIMESTAMP NOT NULL,
   filename TEXT NOT NULL,
-  description TEXT NOT NULL,
-  FOREIGN KEY (usr_id) REFERENCES user (id)
+  description TEXT
 );
 
 -- Create the subscriptions table
 CREATE TABLE IF NOT EXISTS subscriptions(
-  subscription_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  subscription_id INTEGER PRIMARY KEY,
   user_id TEXT NOT NULL,
-  subscription_type TEXT NOT NULL,
+  subscription_type TEXT,
   endpoint_url TEXT NOT NULL,
   DB_url TEXT NOT NULL,
   query TEXT,
   interval TEXT,
   active BOOLEAN NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- Trigger for updating the 'updated_at' timestamp
-CREATE OR REPLACE FUNCTION update_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = CURRENT_TIMESTAMP;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
 
--- Apply the trigger to the subscriptions table
-CREATE TRIGGER update_subscriptions_updated_at
-BEFORE UPDATE ON subscriptions
-FOR EACH ROW
-EXECUTE FUNCTION update_timestamp();
+CREATE TRIGGER update_subscription_timestamp
+AFTER UPDATE ON subscriptions
+BEGIN
+    UPDATE subscriptions SET updated_at = CURRENT_TIMESTAMP WHERE rowid = NEW.rowid;
+END;
+
+
