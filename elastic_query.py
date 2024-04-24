f1eb975890db613a2bcee498ee13fbdb5f78f4f2
midan171 <mikedan171@gmail.com>
2024-04-24 16:27:30 +0300

    DFF_Elasticsearch_SQLite_

---
+++ elastic_query.py
@@ -0,0 +1,66 @@
+import requests
+import logging
+from datetime import datetime, timedelta
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+#thoughts excerpt
+class ElasticQuery:
+    def __init__(self, es_url, index, query, headers, endpoint, interval=10):
+        if not all([es_url, index, query, headers, endpoint]):
+            raise ValueError("All parameters must be provided and non-empty.")
+
+        self.es_url = es_url
+        self.index = index
+        self.query = query
+        self.endpoint = endpoint
+        self.headers = headers
+        self.interval = timedelta(seconds=interval)
+        self.last_run = None
+        
+        print("ES URL: ", es_url)
+        print("Index: ", index)
+        print("Query: ", query)
+        print("Headers: ", headers)
+        print("Endpoint: ", endpoint)
+        print("Interval: ", interval)
+        print("Last Run: ", self.last_run)
+
+    def run_query(self):
+        url = f"{self.es_url}/{self.index}/_search"
+        try:
+            response = requests.post(url, json=self.query, headers=self.headers)
+            if response.status_code == 200:
+                logging.info("=========Query executed successfully=========")
+                return response.json()
+            else:
+                logging.error("Failed to execute query with status code %s", response.status_code)
+                return None
+        except Exception as e:
+            logging.error("=========Failed to execute query=========")
+            return None
+
+    def post_results(self, results):
+        """Posts query results to a specified endpoint."""
+        if not results or 'hits' not in results or not results['hits'].get('hits', []):
+            logging.warning("No results to post or empty results.")
+            return None
+
+        try:
+            response = requests.post(self.endpoint, json=results, headers=self.headers, timeout=6)
+            if response.status_code == 200:
+                logging.info("Results successfully posted.")
+            else:
+                logging.warning("Failed to post results: HTTP %s", response.status_code)
+            return response.status_code
+        except Exception as e:
+            logging.error("=========Error posting results=========")
+            return None
+
+    def print_results(self, results):
+        """Prints query results if available."""
+        if results:
+            print(results)
+        else:
+            logging.info("No results available to print.")
--- subscriptions_manager.py
+++ subscriptions_manager.py
@@ -1,49 +1,42 @@
 from flask import current_app
 import json
 import os
+from elastic_query import ElasticQuery
 
 # Set important paths
 ES_ALERT_SYSTEM_PATH = './ES_alert_system'
 CONFIG_FILE_PATH = os.path.join(ES_ALERT_SYSTEM_PATH,'config.json')
 ES_INDEX = 'test_index'
 
+class ConfigReader:
+    @staticmethod
+    def read_config(config_file):
+        with open(config_file, 'r') as file:
+            data = json.load(file)
+            return [ElasticQuery(**item) for item in data["rules"]]
+
 
 def add_subscription(subscription_id, user_id, subscription_type, endpoint_url, DB_url, query, interval, active):
-    try:
         if subscription_type == 'ES':
             try:
                 current_app.logger.debug("Open config file " + CONFIG_FILE_PATH + " for reading")
-                with open(CONFIG_FILE_PATH, 'r') as file:
-                    try:
-                        data = json.load(file)
-                    except json.JSONDecodeError as e:
-                        current_app.logger.debug("In subscriptionSubmission view " + str(e))
-                        # Initialize data with a default structure if file is empty or not properly formatted
-                        data = {'rules': []}
-                    
-                    # Append the new subscription rule
-                    data['rules'].append({
-                        "subscription_id": subscription_id,
-                        "user_id": user_id,
-                        "es_url": DB_url,
-                        "index": ES_INDEX,
-                        "query": query,
-                        "headers": {"Content-Type": "application/json"},
-                        "endpoint": endpoint_url,
-                        "interval": interval
-                    })
+                queries = ConfigReader.read_config('./ES_alert_system/config.json')
+                
+                new_query = ElasticQuery(DB_url, ES_INDEX, query, {"Content-Type": "application/json"}, endpoint_url, interval)
+                
+                queries.append(new_query)
+                
+                #write the queries back to the config file
+                with open(CONFIG_FILE_PATH, 'w') as file:
+                    json.dump(data, file, indent=4)
+                    current_app.logger.debug("Config file updated with new subscription.")
+                return 'Subscription added successfully'
+            
             except Exception as e:
                 current_app.logger.debug("An unexpected error occurred: " + str(e))
-            
-            # Write the updated data back to the file
-            with open(CONFIG_FILE_PATH, 'w') as file:
-                json.dump(data, file, indent=4)
-                current_app.logger.debug("Config file updated with new subscription.")
-
-    except EOFError as e:
-        return str(e)
+                return str(e)
     
-    return 'Subscription stored successfully'
+        
 def update_subscription(subscription_id, user_id, subscription_type, endpoint_url, DB_url, query, interval, active):
     try:
         # Update the ./ES_alert_system/config.json file with the new subscription if subscription type is 'ES'
