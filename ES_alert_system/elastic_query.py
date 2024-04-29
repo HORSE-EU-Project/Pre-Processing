bbd35aff3ea88e386b4daf4a6370e63386ee6f07
midan171 <mikedan171@gmail.com>
2024-04-29 14:58:03 +0300

    DFF_Elasticsearch_SQLite_

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -32,15 +32,17 @@ class ElasticQuery:
     def run_query(self):
         url = f"{self.es_url}/{self.index}/_search"
         try:
+            
             #unfold the string query to a dictionary
-            q = self.query["query"]
-            key, value = q.popitem()
-            q = {str(key): value}
             qry = {
                     "query": {
                         "match_all": {}
                     }
                 }
+            
+            logging.info("=========Executing query=========")
+            logging.info("Query: %s", qry)
+            logging.info("URL: %s", url)
             
             response = requests.post(url, json=qry, headers=self.headers)
             if response.status_code == 200:
