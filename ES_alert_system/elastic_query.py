877e137378c8c2d892821af459825e12878ad52d
midan171 <mikedan171@gmail.com>
2024-04-29 13:18:01 +0300

    DFF_Elasticsearch_SQLite_

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 #thoughts excerpt
 class ElasticQuery:
     def __init__(self, subscription_id, user_id, subscription_type='ES', es_url='', index='test_index', query='', headers='', endpoint='', interval=10):
-        if not all([es_url, index, query, headers, endpoint]):
+        if not all([es_url, index, query, headers, _url]):
             raise ValueError("All parameters must be provided and non-empty.")
         self.subscription_id = subscription_id
         self.user_id = user_id
