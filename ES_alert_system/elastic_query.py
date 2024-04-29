e0099b699cb14b7b19dc393072e631a701915e5d
midan171 <mikedan171@gmail.com>
2024-04-29 16:27:42 +0300

    DFF_Elasticsearch_SQLite_

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -46,10 +46,10 @@ class ElasticQuery:
             logging.info("URL: %s", url)
             
             logging.info("=========Executing query 22222=========")
-            logging.info("Query: %s", dict(self.query))
+            logging.info("Query: %s", {self.query})
             logging.info("URL: %s", url)
             
-            response = requests.post(url, json=dict(self.query), headers=self.headers)
+            response = requests.post(url, json={self.query}, headers=self.headers)
             if response.status_code == 200:
                 logging.info("=========Query executed successfully=========")
                 return response.json()
