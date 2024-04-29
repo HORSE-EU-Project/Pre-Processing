6dea380d78c9861a5aa6e6356a2ecf1b696f6558
midan171 <mikedan171@gmail.com>
2024-04-29 16:36:09 +0300

    DFF_Elasticsearch_SQLite_

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -46,7 +46,8 @@ class ElasticQuery:
             logging.info("URL: %s", url)
             
             logging.info("=========Executing query 22222=========")
-            logging.info("Query: %s", json.loads(str(self.query)))
+            logging.info("Query type: %s", type(self.query))
+            logging.info("Query: %s", self.query)
             logging.info("URL: %s", url)
             
             response = requests.post(url, json={self.query}, headers=self.headers)
