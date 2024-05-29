92ba73ef3fa753cad5cfb6fa3f35ef7d4939c07e
midan171 <mikedan171@gmail.com>
2024-05-29 15:51:31 +0300

    Changes client kc credentials

--- Web_app/decoratorApp.py
+++ Web_app/decoratorApp.py
@@ -12,5 +12,5 @@ def decoratorCheckAppOrg(func):
         # if current_user.is_authenticated and (User.get_field("user_id", current_user.id, "apps", "name")==-1 or User.get_field("id", current_user.id, "user", "organization")==None):
         #     return render_template('modal.html' , name = current_user.name, email = current_user.email)
         # else:
-        return func()
+            return func()
     return inner    
