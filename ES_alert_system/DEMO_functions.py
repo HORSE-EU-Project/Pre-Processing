80fc407a5243cab90d44cd796c7785ced4f8f103
midan171 <mikedan171@gmail.com>
2024-07-10 12:09:11 +0300

    Demo functions

--- ES_alert_system/DEMO_functions.py
+++ ES_alert_system/DEMO_functions.py
@@ -11,26 +11,6 @@ DEME_url = 'http://your-api-endpoint'
 NKUA_DTE_url = 'http://91.138.223.127'
 
 
-
-def adjust_file_permissions(file_path):
-    try:
-        os.chmod(file_path, 0o644)  # Readable and writable by owner, readable by others
-        logging.info(f"Permissions adjusted for file: {file_path}")
-    except Exception as e:
-        logging.info(f"Failed to adjust permissions for file {file_path}: {e}")
-
-def capture_network(interface, output_path, capture_duration=60):
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    command = ["sudo", "tshark", "-a", f"duration:{capture_duration}", "-i", interface, "-w", output_path]
-    
-    try:
-        logging.info(f"Starting network capture on interface {interface}...")
-        subprocess.run(command, check=True)
-        logging.info(f"Capture completed. Output saved to: {output_path}")
-        adjust_file_permissions(output_path)  # Adjust permissions after capture
-    except subprocess.CalledProcessError as e:
-        logging.info(f"An error occurred during capture: {e}")
-
 def count_pfcp_messages(pcap_file):
     message_counts = {
         "PFCPHeartbeatRequest_counter": 0,
