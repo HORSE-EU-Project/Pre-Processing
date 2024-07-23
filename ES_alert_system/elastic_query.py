c05f194542e4f14c9da77f53952f161221f56130
midan171 <mikedan171@gmail.com>
2024-07-23 13:38:03 +0300

    Fixed docker-compose args

--- ES_alert_system/elastic_query.py
+++ ES_alert_system/elastic_query.py
@@ -3,14 +3,19 @@ import requests
 import logging
 from datetime import datetime, timedelta
 import DEMO_functions as fun
+import time
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+ES_username = 'elastic'
+ES_password = 'HoR$e2024!eLk@sPh#ynX'
+
 #thoughts excerpt
 class ElasticQuery:
-    def __init__(self, subscription_id, user_id, subscription_type='ES', DB_url='', index='test_index', 
-                 query='', headers = {"Content-Type": "application/json"}, endpoint_url='', interval=10, active=True, **kwargs):
+    def __init__(self, subscription_id, user_id, subscription_type='ES', DB_url='', index='packets-2024-07-09', 
+                 query='', query_type = '_count', headers = {"Content-Type": "application/json"}, endpoint_url='', interval=10, active=True, 
+                 username=ES_username, password=ES_password, **kwargs):
         self.subscription_id = subscription_id
         self.user_id = user_id
         self.subscription_type = subscription_type
@@ -21,7 +26,13 @@ class ElasticQuery:
         self.headers = headers
         self.interval = timedelta(seconds= int(interval))
         self.active = active
-        self.last_run = None
+        #put in the self.last_run the datetime 2024-07-09T09:11:37.162609000Z
+        self.last_run = datetime(2024, 7, 9, 9, 11, 37, 162609)
+        self.previous_last_run = self.last_run - self.interval
+        
+        self.username = username
+        self.password = password
+        self.query_type = query_type
         
         print("ES URL: ", self.es_url)
         print("Index: ", self.index)
@@ -32,100 +43,122 @@ class ElasticQuery:
         print("Last Run: ", self.last_run)
 
     def run_query(self):
-        #if es_url is "DEME" then read from "a local pcap file" else read from "ES"
-        if self.es_url == "DEME":
-            return "OK"
-        elif self.es_url == "NKUA-DTE":
-            #using nkua functions to read from a local pcap file and send the results to the endpoint
-            fun.process_capture_in_background("test.pcap")
-            return "OK"
-        else:
-            url = f"{self.es_url}/{self.index}/_search"
-            try:
-                qry = {"query": self.query}
-                
+        url = f"{self.es_url}/{self.index}/{self.query_type}"
+        self.previous_last_run = self.last_run
+        
+        #put in the self.last_run the previous last run + interval
+        self.last_run = self.previous_last_run + self.interval
+        
+        #Only for the demo #1
+        self.query = self.set_query_time_window(self.previous_last_run, self.last_run, self.query)
+        
+        try:
+            # Convert query string to dictionary if necessary
+            if isinstance(self.query, str):
+                qry = json.loads(self.query.replace("'", '"'))
+            else:
+                qry = self.query
 
+            logging.info("=========================== Executing query ===========================")
+            # logging.info("Query: %s", json.dumps(qry, indent=2))
+            # logging.info("URL: %s", url)
 
-                # If the query is a string, convert it to a dictionary
-                key,value = qry["query"].split(":", 1)
-                qry["query"] = {key:eval(value.strip())}
-                
-                logging.info("=========================== Executing query ===========================")
-                logging.info("Query: %s", str(json.dumps(qry)))
-                logging.info("URL: %s", url)
-                
-                response = requests.post(url, data=json.dumps(qry), headers=self.headers)
-                
-                if response.status_code == 200:
-                    logging.info("=========Query executed successfully=========")
-                    return response.json()
-                else:
-                    logging.error("Failed to execute query with status code %s", response.status_code)
-                    return None
-            except Exception as e:
-                logging.error("=========Failed to execute query=========",str(e))
+            response = requests.post(url, data=json.dumps(qry), headers=self.headers, auth=(self.username, self.password))
+
+            if response.status_code == 200:
+                logging.info("=========Query executed successfully=========")
+                return response.json()
+            else:
+                logging.error("Failed to execute query with status code %s", response.status_code)
+                logging.error("Response: %s", response.text)
                 return None
+        except Exception as e:
+            logging.error("=========Failed to execute query=========", str(e))
+            return None
+
+
 
     def post_results(self, results):
-        """Posts query results to a specified endpoint."""
-        if not results or 'hits' not in results or not results['hits'].get('hits', []):
-            logging.warning("No results to post or empty results.")
-            return None
-
-        try:
-            response = requests.post(self.endpoint, json=results, headers=self.headers, timeout=6)
-            if response.status_code == 200:
-                logging.info("Results successfully posted.")
-            else:
-                logging.warning("Failed to post results: HTTP %s", response.status_code)
-            return response.status_code
-        except Exception as e:
-            logging.error("=========Error posting results=========")
-            return None
-
-    def print_results(self, results):
-        """Prints query results if available."""
+        #if results are available print them and then post them, else print a message    
         if results:
-            print(results)
+            
+            if self.endpoint == 'http://192.168.130.110:8090/estimate':
+                results = self.DEME_transformation(results)
+                print(results)
+            try:
+                
+                response = requests.post(self.endpoint, json=results, headers=self.headers)
+                if response.status_code == 200:
+                    logging.info("Results successfully posted.")
+                else:
+                    logging.warning("Failed to post results: HTTP %s", response.status_code)
+                return response.status_code
+            except Exception as e:
+                logging.error("=========Error posting results=========")
+                return None
         else:
-            logging.info("No results available to print.")
-
+            logging.info("No results available to post.")
     
-    def convert_query_string(input_string):
-        try :
-            # Strip leading and trailing spaces and remove the initial "query:" part
-            json_part = input_string.split("query:", 1)[1].strip()
-
-            # Properly format the string for JSON conversion by replacing single quotes if necessary
-            json_part = json_part.replace('\'', '\"')
-
-            # Convert the string to a dictionary
-            query_dict = json.loads(json_part)
-
-            # Create the final dictionary expected
-            result = {"query": query_dict}
-
-            # Return the result as a JSON string for clarity and to match the expected output format
-            return json.dumps(result)
-        
-        except Exception as e:
-            # Handle exceptions that may arise from incorrect formatting or parsing
-            return str(e)
-        
-    def execute_DEME(self):
-        # Read from a local pcap file
-        logging.info("Reading from DEME")
-        response_json = fun.read_pcap_for_DEME("test.pcap")
-        fun.send_data_to_DEME(response_json)
-        return None
     
-    def execute_NKUA_DTE(self):
-        logging.info("Reading from NKUA-DTE")
+    def set_query_time_window(self, previous_last_run, last_run, query):
+        # Convert datetime objects to ISO format strings if they are not already
+        if isinstance(previous_last_run, datetime):
+            previous_last_run = previous_last_run.isoformat() + 'Z'
+        if isinstance(last_run, datetime):
+            last_run = last_run.isoformat() + 'Z'
+
+        # Update the query object with the new timestamps
+        for clause in query['query']['bool']['must']:
+            if 'range' in clause and 'layers.frame.frame_frame_time' in clause['range']:
+                clause['range']['layers.frame.frame_frame_time']['gte'] = previous_last_run
+                clause['range']['layers.frame.frame_frame_time']['lt'] = last_run
+
+        return query
+
+    def DEME_transformation(self, results):
+        # Extract counts from the results
+        dns_count = results['aggregations']['dns_packets']['doc_count']
+        ntp_count = results['aggregations']['ntp_packets']['doc_count']
         
-        # use nkua functions to read from a local pcap file
-        message_counts = fun.count_pfcp_messages("test.pcap")
+        # Convert the last_run datetime to a Unix timestamp (seconds since the epoch)
+        timestamp_unix = int(time.mktime(self.last_run.timetuple()))
         
-        #send the message counts to the endpoint
-        fun.send_to_bentoml(message_counts)
+        # Transform the results to the DEME API format
+        transformed_results = [
+            {
+                "timestamp": str(timestamp_unix),
+                "instances": [
+                    {
+                        "instance": "node1:Genoa RtA",
+                        "features": [
+                            {
+                                "feature": "NTP",
+                                "value": ntp_count
+                            },
+                            {
+                                "feature": "DNS",
+                                "value": dns_count
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
         
-        return None
+
+        
+        return transformed_results
+
+
+# Example usage
+if __name__ == "__main__":
+    endpoint = "http://192.168.130.110:8090/estimate"
+    index = "packets-2024-07-09"
+    headers = {'Content-Type': 'application/json'}
+    username = "elastic"
+    password = "HoR$e2024!eLk@sPh#ynX"
+    results = [{'timestamp': '1720507497', 'instances': [{'instance': 'node1:Genoa RtA', 'features': [{'feature': 'NTP', 'value': 0}, {'feature': 'DNS', 'value': 698}]}]}]
+    
+    response = requests.post(endpoint, json=results, headers=headers)
+    print(response.status_code)
+    
