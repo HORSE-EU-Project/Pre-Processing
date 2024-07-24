8b2d514affae4a04d1b2f0dd807c03a067847559
midan171 <mikedan171@gmail.com>
2024-07-24 15:20:58 +0300

    Fixed timewindow 2m and finishing at specific time at the end of the file

--- ES_alert_system/main.py
+++ ES_alert_system/main.py
@@ -37,7 +37,7 @@ def main():
         now = datetime.now()
         for query in queries.ES_queries:
             print("=========================++++++====================================")
-            if not query.last_run or now >= query.last_run + query.interval:
+            if (not query.last_run or now >= query.last_run + query.interval) and query.active:
                 try:
                     results = query.run_query()
                     status_code = query.post_results(results)  
@@ -53,10 +53,13 @@ def main():
                     logging.error(f"HTTP error occurred: {e}")
                 except Exception as e:
                     logging.error(f"An unexpected error occurred: {e}")
-                    
+            #if query.last_run is larger that 2024-07-09T16:09:37.162609000Z print the message
+            if query.last_run > datetime(2024, 7, 9, 9, 15, 37, 162609):
+                print("==========================Query reached the end of the data============================")
+                query.active = False        
                 #query.last_run = now
         print("=============================================================")
-        time.sleep(10)
+        time.sleep(5)
         
 
 
